d54d8325e6c66e3cf2632f24280125d0fdfccfe8
Matt W <tashvelocity@gmail.com>
2025-02-16 22:15:17 +0000

    Added empty mapcycle.txt file to prevent map rotation

--- configs.sh
+++ configs.sh
@@ -7,22 +7,22 @@ cd $HOME/tf/cfg
 cp -n /server.cfg.template ./server.cfg
 
 #create empty map cycle to stop map rotation
-cp -n /mapcycle.txt ./mapcycle.txt
+printf "" >> ./mapcycle.txt
 
-ugc_cfg_version_9=$(wget -q -O - "https://www.ugcleague.com/files_tf2h.cfm" | grep -oP "UGC_HL_cfg_(.*)\.zip" | grep -m1 -oP "v\d{6,8}")
-ugc_cfg_version_6=$(wget -q -O - "https://www.ugcleague.com/files_tf26.cfm" | grep -oP "UGC_6v6_cfg_(.*)\.zip" | grep -m1 -oP "v\d{6,8}")
-ugc_cfg_version_4=$(wget -q -O - "https://www.ugcleague.com/files_tf24.cfm" | grep -oP "UGC_4v4_cfg_(.*)\.zip" | grep -m1 -oP "v\d{6,8}")
+ugc_cfg_version_9=$(wget -nv -q -O - "https://www.ugcleague.com/files_tf2h.cfm" | grep -oP "UGC_HL_cfg_(.*)\.zip" | grep -m1 -oP "v\d{6,8}")
+ugc_cfg_version_6=$(wget -nv -q -O - "https://www.ugcleague.com/files_tf26.cfm" | grep -oP "UGC_6v6_cfg_(.*)\.zip" | grep -m1 -oP "v\d{6,8}")
+ugc_cfg_version_4=$(wget -nv -q -O - "https://www.ugcleague.com/files_tf24.cfm" | grep -oP "UGC_4v4_cfg_(.*)\.zip" | grep -m1 -oP "v\d{6,8}")
 
 wget -nv "https://github.com/ETF2L/gameserver-configs/releases/latest/download/etf2l_configs.zip"
-unzip -o etf2l_configs.zip
+unzip -o -qq etf2l_configs.zip
 
 wget -nv --header "User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11" "https://www.ugcleague.com/files/configs/UGC_HL_cfg_${ugc_cfg_version_9}.zip"
 wget -nv --header "User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11" "https://www.ugcleague.com/files/configs/UGC_6v6_cfg_${ugc_cfg_version_6}.zip"
 wget -nv --header "User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11" "https://www.ugcleague.com/files/configs/UGC_4v4_cfg_${ugc_cfg_version_4}.zip"
 
-unzip -jo UGC_HL_cfg_${ugc_cfg_version_9}.zip
-unzip -jo UGC_6v6_cfg_${ugc_cfg_version_6}.zip
-unzip -jo UGC_4v4_cfg_${ugc_cfg_version_4}.zip
+unzip -jo -qq UGC_HL_cfg_${ugc_cfg_version_9}.zip
+unzip -jo -qq UGC_6v6_cfg_${ugc_cfg_version_6}.zip
+unzip -jo -qq UGC_4v4_cfg_${ugc_cfg_version_4}.zip
 
 # dont set the server and stv name
 sed -i '/hostname/d' ugc_*_custom.cfg

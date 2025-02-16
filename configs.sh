e527d9745bfdb4a51e1d6ec2f9ff5238b7b7ba03
Matt W <tashvelocity@gmail.com>
2025-02-16 17:45:35 +0000

    Added option to force overwriting of etf2l_configs

--- configs.sh
+++ configs.sh
@@ -8,15 +8,15 @@ ugc_cfg_version_6=$(wget -q -O - "https://www.ugcleague.com/files_tf26.cfm" | gr
 ugc_cfg_version_4=$(wget -q -O - "https://www.ugcleague.com/files_tf24.cfm" | grep -oP "UGC_4v4_cfg_(.*)\.zip" | grep -m1 -oP "v\d{6,8}")
 
 wget -nv "https://github.com/ETF2L/gameserver-configs/releases/latest/download/etf2l_configs.zip"
-unzip etf2l_configs.zip
+unzip -o etf2l_configs.zip
 
 wget -nv --header "User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11" "https://www.ugcleague.com/files/configs/UGC_HL_cfg_${ugc_cfg_version_9}.zip"
 wget -nv --header "User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11" "https://www.ugcleague.com/files/configs/UGC_6v6_cfg_${ugc_cfg_version_6}.zip"
 wget -nv --header "User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11" "https://www.ugcleague.com/files/configs/UGC_4v4_cfg_${ugc_cfg_version_4}.zip"
 
-unzip -oj UGC_HL_cfg_${ugc_cfg_version_9}.zip
-unzip -oj UGC_6v6_cfg_${ugc_cfg_version_6}.zip
-unzip -oj UGC_4v4_cfg_${ugc_cfg_version_4}.zip
+unzip -jo UGC_HL_cfg_${ugc_cfg_version_9}.zip
+unzip -jo UGC_6v6_cfg_${ugc_cfg_version_6}.zip
+unzip -jo UGC_4v4_cfg_${ugc_cfg_version_4}.zip
 
 # dont set the server and stv name
 sed -i '/hostname/d' ugc_*_custom.cfg
